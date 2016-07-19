e8dc5d5d58761599051c01218e6e5ec50bc36c31
Boris Kaul <localvoid@gmail.com>
2016-07-19 12:58:19 +0600

    Update to 0.14.8, refactor, switch hosting to rawgit

---
+++ js/main.jsx
@@ -0,0 +1,179 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TableCell extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.text !== nextProps.text;
+  }
+
+  onClick(e) {
+    console.log('Clicked' + this.props.text);
+    e.stopPropagation();
+  }
+
+  render() {
+    return <td className="TableCell" onClick={this.onClick}>{this.props.text}</td>;
+  }
+}
+
+class TableRow extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.data !== nextProps.data;
+  }
+
+  render() {
+    const { data } = this.props;
+
+    // Interned strings
+    const classes = (data.active) ? 'TableRow active' : 'TableRow';
+
+    const cells = data.props;
+
+    const children = [];
+    for (let i = 0; i < cells.length; i++) {
+      // Key is used because React prints warnings that there should be a key, libraries that can detect that children
+      // shape isn't changing should render cells without keys.
+      children.push(<TableCell key={i} text={cells[i]}></TableCell>);
+    }
+
+    // First table cell is inserted this way to prevent react from printing warning that it doesn't have key property
+    return <tr className={classes} data-id={data.id}><TableCell text={'#' + data.id}></TableCell>{children}</tr>;
+  }
+}
+
+class Table extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.data !== nextProps.data;
+  }
+
+  render() {
+    const items = this.props.data.items;
+
+    const children = [];
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      children.push((<TableRow key={item.id} data={item} />));
+    }
+
+    return <table className="Table"><tbody>{children}</tbody></table>;
+  }
+}
+
+class AnimBox extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.data !== nextProps.data;
+  }
+
+  render() {
+    const { data } = this.props;
+    const time = data.time;
+    const style = {
+      'borderRadius': (time % 10).toString() + 'px',
+      'background': 'rgba(0,0,0,' + (0.5 + ((time % 10) /10)).toString() + ')'
+    };
+
+    return <div className="AnimBox" data-id={data.id} style={style} />;
+  }
+}
+
+class Anim extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.data !== nextProps.data;
+  }
+
+  render() {
+    const items = this.props.data.items;
+
+    const children = [];
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      children.push(<AnimBox key={item.id} data={item} />);
+    }
+
+    return <div className="Anim">{children}</div>;
+  }
+}
+
+class TreeLeaf extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.data !== nextProps.data;
+  }
+
+  render() {
+    return <li className="TreeLeaf">{this.props.data.id}</li>;
+  }
+}
+
+class TreeNode extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.data !== nextProps.data;
+  }
+
+  render() {
+    const { data } = this.props;
+    const children = [];
+
+    for (let i = 0; i < data.children.length; i++) {
+      const n = data.children[i];
+      if (n.container) {
+        children.push(<TreeNode key={n.id} data={n} />);
+      } else {
+        children.push(<TreeLeaf key={n.id} data={n} />);
+      }
+    }
+
+    return <ul className="TreeNode">{children}</ul>;
+  }
+}
+
+class Tree extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.data !== nextProps.data;
+  }
+
+  render() {
+    return <div className="Tree"><TreeNode data={this.props.data.root} /></div>;
+  }
+}
+
+class Main extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.data !== nextProps.data;
+  }
+
+  render() {
+    const { data } = this.props;
+    const location = data.location;
+
+    var section;
+    if (location === 'table') {
+      section = <Table data={data.table}></Table>;
+    } else if (location === 'anim') {
+      section = <Anim data={data.anim}></Anim>;
+    } else if (location === 'tree') {
+      section = <Tree data={data.tree}></Tree>;
+    }
+
+    return <div className="Main">{section}</div>;
+  }
+}
+
+uibench.init('React', '0.14.8');
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  const container = document.querySelector('#App');
+
+  uibench.run(
+    function(state) {
+      ReactDOM.render(<Main data={state}/>, container);
+    },
+    function(samples) {
+      ReactDOM.render(<pre>{JSON.stringify(samples, null, ' ')}</pre>, container);
+    }
+  );
+});
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, 'js', 'main.jsx'),
+    fc: path.join(__dirname, 'js', 'fc.jsx'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: ['babel?{ "presets": ["es2015-loose", "react"], "plugins": ["transform-react-inline-elements"] }']
+    }]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+};
